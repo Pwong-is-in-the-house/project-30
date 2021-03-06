41684dce8ce337c18769ae56afecd6d2605c44d7
Pwong-is-in-the-house <45134579+Pwong-is-in-the-house@users.noreply.github.com>
2021-03-06 11:54:42 -0500

    Update Box.js

--- Box.js
+++ Box.js
@@ -1,17 +1,30 @@
 class Box{
     constructor(x,y,color){
-       
-this.body=Bodies.rectangle(x,y,30,50)
-this.Visibility= 255;
+       var options={
+           restitution:0.4,
+           friction:0.0
+       }
+       this.Visibility= 255;
+       this.body=Bodies.rectangle(x,y,30,50)
+this.width=width;
+this.height=height
 World.add(world,this.body)
     }
 display(){
+    if(this.body.speed<5){
+    var angle=this.body.angle
+   var pos=this.body.position;
     push();
+    translate(pos.x,pos.y);
+    rotate(angle);
     rectMode(CENTER)
-    rect(this.body.position.x,this.body.position.y,30,50)
-    this.Visibility= this.Visibility - 5;
-    tint(255,this.Visibility);
-    image(this.image, this.body.x, this.body.position.y,50,50);
-    pop();
+    rect(0,0,this.width,this.height);
+    pop();}
+    else{ 
+        World.remove(world,this,body);
+        push();
+        this.Visibility= this.Visibility - 5;
+pop();
+    }
 }
 }
